c9338b9305dd8f399733fea8f2769883b1959017
Chernyi Evgeniy <archi.xiii@gmail.com>
2016-12-11 20:08:56 +0400

    corrected import

---
+++ webstore/src/main/java/com/ncproject/webstore/controller/CustomerServlet.java
@@ -0,0 +1,19 @@
+package com.ncproject.webstore.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by admin on 11.12.2016.
+ */
+@WebServlet("/Customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
